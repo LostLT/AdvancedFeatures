d47105cb828a2ff23493909b77dadc1f72e815dc
LostLT <maladuir@yahoo.com>
2015-01-08 15:27:00 -0600

    script changes 1
    added:
    -preexportmethod
    -postexportmethod
    -remove scene 2: what happens?

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -3,14 +3,35 @@ using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+	public static void PreExportMethod () {
+
+		Debug.Log("******THE PREEXPORT TOOL WORKS!!******");
+
+	}
+
+	public static void PostExportMethod () {
+		
+		Debug.Log("******THE POSTEXPORT TOOL WORKS!!******");
+		
+	}
+
 	// Use this for initialization
 	void OnGUI () {
 
 		GUI.Label (new Rect (250, 70, 100, 20), "This is Scene 1");
 		if (GUI.Button (new Rect (250, 100, 100, 25), "Go to Scene 2"))
-			Application.LoadLevel ("Scene 2");
+		{
+			try 
+			{
+				Application.LoadLevel ("Scene 2");
+			}
+			catch (System.Exception e) 
+			{
+				GUI.Label (new Rect (250, 100, 100, 20), "There is no Scene 2!");
+			}
+		}
 
-		#if CLOUD_BUILD
+		#if UNITY_CLOUD_BUILD
 
 			GUI.Label(new Rect(10,10,225,20), "This ad has been brought to you by the people at CloudBuild.");
 
