a5506864cd04fd3ac4ec135b14d2578a56ea62bc
LostLT <maladuir@yahoo.com>
2015-09-17 13:37:35 -0500

    new stuff

---
+++ AdvancedFeatures3/Assets/Post.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Post : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static void Export() {
+
+		Debug.Log ("Post-Export fired");
+
+	}
+}
